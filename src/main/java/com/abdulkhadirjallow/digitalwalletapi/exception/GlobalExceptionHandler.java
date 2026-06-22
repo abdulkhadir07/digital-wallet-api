32706c501076d3c20fb4946974ae8327f480d1f0
Abdul Khadir <abdulkhadirjallow743@gmail.com>
2026-06-22 01:52:50 -0400

    chore(logging): log unexpected server errors

--- src/main/java/com/abdulkhadirjallow/digitalwalletapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/abdulkhadirjallow/digitalwalletapi/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.abdulkhadirjallow.digitalwalletapi.exception;
 
 import com.abdulkhadirjallow.digitalwalletapi.dto.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,6 +17,8 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(UnauthorizedException.class)
 
@@ -65,6 +69,9 @@ public class GlobalExceptionHandler {
     // Unexpected Errors
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
+
+        log.error("Unexpected error occurred", ex);
+
         ErrorResponse errorResponse = new ErrorResponse(
                 "An unexpected error occurred",
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
